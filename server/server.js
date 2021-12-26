18c3136d941e77a2fdb7f136677d2ee083947d49
Brian <briansobrian@gmail.com>
2021-12-25 22:23:09 -0500

    Adding semi colon

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const port = process.env.port || 3000;
 
 // dist contains our webpack-bundled html and js files
 // set the node environment to production
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static('dist'));
+}
 
 app.listen(port, e => {
     if (e) throw e;
