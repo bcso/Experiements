a3e6075eeb3a7b4e26303f3704817a7d55d666a8
Brian <briansobrian@gmail.com>
2021-12-26 21:07:35 -0500

    moving about

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -4,6 +4,7 @@ function About() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>This is the about page</h2>
+        <Link to="/"> <Button> Home </Button> </Link>
       </main>
     );
 }
