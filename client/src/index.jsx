62ae315b0966ed154f0aa6bca1f24f14f01f8dff
Brian <briansobrian@gmail.com>
2021-12-26 21:06:32 -0500

    Un nesting routes for about and games

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import TickTacToe from './components/Games/tictactoe';
+import TicTacToe from './components/Games/tictactoe';
 import About from './components/Games/about';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App/>}>
-                <Route path="ttc" element={<TickTacToe/>} />
-                <Route path="about" element={<About />} />
-            </Route>
+            <Route path="/" element={<App/>}> </Route>
+            <Route path="ttc" element={<TicTacToe/>} />
+            <Route path="about" element={<About />} />
             
             {/* Navigate back to home for any routes we don't recognize */}
             <Route path="*" element={<Navigate to="/" />}/>
