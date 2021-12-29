15bc1d9ddb1d95b3d8c737bde561909b81e82d6d
Brian <briansobrian@gmail.com>
2021-12-28 19:03:10 -0500

    Main page changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,33 +8,25 @@ import {
     Col,
     Button
 } from 'react-bootstrap';
+import PageBaseLayout from './common/PageBaseLayout';
 
 function App(){
     return(
+        <PageBaseLayout>
+
         <div className={styles.introPageRoot}>
             <Container className={styles.introPageContainer} fluid>
                 <Row className={styles.introPageTitle}>
-                    <Col> <h1>Game App</h1> </Col>
+                    <Col> <h1 className='display-1'>Experiments</h1> </Col>
                 </Row>
-                <Row className={styles.introPageButtonNav}>
-                    <Col xs="auto">
-                        <Link to="/toe">
-                            <Button size='lg'> TicTacToe </Button>
-                        </Link>
-                    </Col>
-                    <Col xs="auto">
-                        <Link to="/chat">
-                            <Button size='lg'> Chat Room </Button>
-                        </Link>
-                    </Col>
-                    <Col xs="auto">
-                        <Link to="/about">
-                            <Button size='lg'> About </Button>
-                        </Link>
+                <Row>
+                    <Col>
+                        <h6 className='lead text-muted'> Some Experiements above... click on them and try.</h6>
                     </Col>
                 </Row>
             </Container>
         </div>
+        </PageBaseLayout>
     )
 }
 
