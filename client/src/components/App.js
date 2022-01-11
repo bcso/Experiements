f8c495d6a5f2f6d037030f52a9c89cbefa9dbf21
Brian <briansobrian@gmail.com>
2022-01-10 20:54:18 -0500

    whitespace

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,19 +13,18 @@ import PageBaseLayout from './common/PageBaseLayout';
 function App(){
     return(
         <PageBaseLayout>
-
-        <div className={styles.introPageRoot}>
-            <Container className={styles.introPageContainer} fluid>
-                <Row className={styles.introPageTitle}>
-                    <Col> <h1 className='display-1'>Experiments</h1> </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h6 className='lead text-muted'> Some Experiements above... click on them and try.</h6>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+            <div className={styles.introPageRoot}>
+                <Container className={styles.introPageContainer} fluid>
+                    <Row className={styles.introPageTitle}>
+                        <Col> <h1 className='display-1'>Experiments</h1> </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h6 className='lead text-muted'> Some Experiements above... click on them and try.</h6>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </PageBaseLayout>
     )
 }
