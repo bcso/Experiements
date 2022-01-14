36da78a8e9364bd90b7b146cae48c2e2ebbaf471
Brian <briansobrian@gmail.com>
2022-01-14 18:50:51 -0500

    Adding vector and base keyboard component

--- client/src/components/Games/Snake/components/SnakeGame.tsx
+++ client/src/components/Games/Snake/components/SnakeGame.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import PageBaseLayout from '../../../common/PageBaseLayout';
 import PageInProgress from '../../../common/PageInProgress';
-import { initSnakeGameBoard } from '../helpers/helpers';
+import { initSnakeGameBoard, initVector } from '../helpers/helpers';
 import Board from './Board';
+import Keypad from './Keypad';
 
 function SnakeGame() {
 
     const [gameBoard, setBoardState] = useState(initSnakeGameBoard());
+    const [currentVector, setCurrentVector] = useState(initVector());
 
     return(
         <PageBaseLayout pageTitle="Snake">
@@ -15,6 +17,7 @@ function SnakeGame() {
                 pageDescription="This is a work in progress page for the Snake game!"/> */}
             
             <Board {...gameBoard} />
+            <Keypad />
         </PageBaseLayout>
     )
 }
