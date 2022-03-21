993e8391c9ff1ab57945cd209fac24d808ab43ec
Brian <briansobrian@gmail.com>
2022-03-21 17:47:15 -0400

    Componentize cell

--- client/src/components/Games/Snake/components/Cell.tsx
+++ client/src/components/Games/Snake/components/Cell.tsx
@@ -1,18 +1,11 @@
 import React from "react";
+import styles from "../css/Cell.module.css";
 
 function Cell({color = "black"}){
     return(
-        <>
-        <div style={
-            {
-                height : "30px",
-                width : "30px",
-                backgroundColor : color,
-                padding: "0px",
-                margin: "1px"
-            }
-        }></div>
-        </>
+        <div className={styles.baseCell} 
+             style={{ backgroundColor : color}}
+        />
     )
 }
 
