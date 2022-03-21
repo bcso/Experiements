9f16517f36bd0911166f8d3f7003e4b1f1463490
Brian <briansobrian@gmail.com>
2022-03-21 17:22:46 -0400

    Add keypad styling

--- client/src/components/Games/Snake/components/Keypad.tsx
+++ client/src/components/Games/Snake/components/Keypad.tsx
@@ -1,7 +1,7 @@
 import React, { EventHandler } from "react";
 import { Button } from "react-bootstrap";
 import { IKeypadProps, Vector } from "../types";
-
+import styles from "../css/Keypad.module.css";
 function Keypad({...KeypadProps} : IKeypadProps) {
 
     function handleKeypadPress(newDirection : string) : React.MouseEventHandler<HTMLButtonElement>
@@ -11,12 +11,12 @@ function Keypad({...KeypadProps} : IKeypadProps) {
     }
 
     return (
-        <>
+        <div className={styles.keypadContainer}>
             <Button onClick={() => handleKeypadPress("Up")}>Up</Button>
             <Button onClick={() => handleKeypadPress("Down")}>Down</Button>
             <Button onClick={() => handleKeypadPress("Right")}>Right</Button>
             <Button onClick={() => handleKeypadPress("Left")}>Left</Button>
-        </>
+        </div>
     )
 }
 
